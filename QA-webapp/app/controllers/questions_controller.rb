92faf6c4c91f4d5fadb70be47f2e979c89f1deff
Samuel Gosselin <samuelgosselin1@hotmail.com>
2018-01-27 21:20:51 -0500

    ask question button & show question

--- QA-webapp/app/controllers/questions_controller.rb
+++ QA-webapp/app/controllers/questions_controller.rb
@@ -3,4 +3,23 @@ class QuestionsController < ApplicationController
     @questions = Question.all
   end
   
+  def new
+  end
+  
+  def create
+    @question = Question.new(question_params) 
+ 
+    @question.save
+    redirect_to questions_path
+  end
+  
+  def show
+    @question = Question.find(params[:id])
+  end
+  
+  private
+    def question_params
+      params.require(:question).permit(:title, :text)
+    end
+  
 end
