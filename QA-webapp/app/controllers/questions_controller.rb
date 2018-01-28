464ceea560967537e35cfe437f83e55334b51c5c
Samuel Gosselin <samuelgosselin1@hotmail.com>
2018-01-28 00:09:36 -0500

    added edit & delete feature

--- QA-webapp/app/controllers/questions_controller.rb
+++ QA-webapp/app/controllers/questions_controller.rb
@@ -1,22 +1,44 @@
 class QuestionsController < ApplicationController
+  
   def index
     @questions = Question.all
   end
   
+  def show
+    @question = Question.find(params[:id])
+  end 
+  
   def new
+  end
+  
+  def edit
+    @question = Question.find(params[:id])
   end
   
   def create
     @question = Question.new(question_params) 
- 
     @question.save
+    
     redirect_to questions_path
   end
   
-  def show
+  def update
     @question = Question.find(params[:id])
+    @question.update(question_params)
+    
+    redirect_to @question
   end
   
+  def destroy
+    @question = Question.find(params[:id])
+    @question.delete
+    
+    redirect_to questions_path
+  end
+  
+  
+  # the objects needs these parameters in order to be saved to the DB
+  # security will be added later
   private
     def question_params
       params.require(:question).permit(:title, :text)
