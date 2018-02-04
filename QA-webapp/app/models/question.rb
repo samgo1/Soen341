375c90214fe2805fb73b9baacec4874c275f793f
Avkash Sharma <avkaashsharma89@gmail.com>
2018-02-04 02:15:44 -0500

    Minor update

--- QA-webapp/app/models/question.rb
+++ QA-webapp/app/models/question.rb
@@ -1,5 +1,5 @@
 class Question < ApplicationRecord
   has_many :responses
   validates :title, presence: true, length: { minimum: 10, maximum: 50 }
-  validates :text, presence: true, length: { minimum: 10, maximum: 300 }
+  validates :text
 end
