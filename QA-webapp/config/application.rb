5df5ca39ab25916dc7307e7ef53b5c1068599b0f
Jeffrey <jeffreytang97@gmail.com>
2018-02-03 23:32:29 -0500

    Date and time feature

--- QA-webapp/config/application.rb
+++ QA-webapp/config/application.rb
@@ -11,8 +11,6 @@ module QAWebapp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-	
-	config.time_zone = 'Eastern Time (US & Canada)'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
