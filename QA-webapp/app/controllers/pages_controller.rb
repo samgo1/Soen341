79db857262d933a0520d8891c839604fc18f2fbd
Nick Zombolas <nickzombolas@gmail.com>
2018-02-07 09:48:25 -0800

    created pages controller, basic signin page

---
+++ QA-webapp/app/controllers/pages_controller.rb
@@ -0,0 +1,6 @@
+class PagesController < ApplicationController
+
+    def signin
+        @title = 'Sign In'
+    end
+end
--- QA-webapp/config/routes.rb
+++ QA-webapp/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   
   root 'questions#index'
+  get 'signin' => 'pages#signin', as: 'signin'
   resources :questions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
