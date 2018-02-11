62a53b4b73d8613a77ab80a4ea306f77ea9619dd
Nick Zombolas <nickzombolas@gmail.com>
2018-02-10 20:54:07 -0800

    User sign up functionality

--- QA-webapp/app/models/user.rb
+++ QA-webapp/app/models/user.rb
@@ -1,10 +1,7 @@
 class User < ApplicationRecord
-
-    #This validation has not been tested yet
-
-    #has_many :questions
-    #validates :username, presence: true
-     #                       length: {minimum: 5}
-    #validates :password, presence: true,
-     #                       length {minimum: 5}
+    has_many :questions
+    validates :username, presence: true,
+                           length: {minimum: 5}
+    validates :password, presence: true,
+                            length: {minimum: 5}
 end
--- QA-webapp/config/routes.rb
+++ QA-webapp/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   
   resources :users
-  root 'welcome#index'
+  root 'welcome#index', as: 'home'
   resources :questions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
