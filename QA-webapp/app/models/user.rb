15e9fac78679c67a878457ad26b015d6fee86bd1
Nick Zombolas <nickzombolas@gmail.com>
2018-02-10 19:49:09 -0800

    User-welcome integration just fixed

--- QA-webapp/app/models/user.rb
+++ QA-webapp/app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+
+    #This validation has not been tested yet
+
+    #has_many :questions
+    #validates :username, presence: true
+     #                       length: {minimum: 5}
+    #validates :password, presence: true,
+     #                       length {minimum: 5}
 end
