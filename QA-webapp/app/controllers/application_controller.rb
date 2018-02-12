25e15973b7c74b50d18953e8347b3425f00ff41d
Khal-Shah <peoul@msn.com>
2018-02-12 03:01:25 -0500

    navbar (with devise gem)

--- QA-webapp/app/controllers/application_controller.rb
+++ QA-webapp/app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  protect_from_forgery prepend: true  
 end
+
+#changed from protect_from_forgery with: exception
