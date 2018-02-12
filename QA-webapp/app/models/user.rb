5b376d3fa783138c872d7e744a51ea5819ea8e9b
Nick Zombolas <nickzombolas@gmail.com>
2018-02-12 13:06:55 -0800

    user sign up validation redone

--- QA-webapp/app/models/user.rb
+++ QA-webapp/app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+    has_many :questions
+    validates :username, presence: true, length: {minimum: 5}
+    validates :password, presence: true, length: {minimum: 5}
 end
