d73ce1f7105800b7883e795c5de7c024d53b1caa
Samuel Gosselin <samuelgosselin1@hotmail.com>
2018-02-12 21:38:56 -0500

    basic authentication method

--- QA-webapp/app/models/user.rb
+++ QA-webapp/app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ApplicationRecord
+  
+  def authenticate
+    existing_user = User.find_by username: self.username
+    if existing_user != nil
+      if existing_user.password == self.password
+        self.id = existing_user.id
+      end
+    end
+    
+    return false
+  end
+  
+  
 end
