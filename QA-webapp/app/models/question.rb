a5f4cb3cc1b3bb5053e999336b4ee0822ee25715
Jeffrey <jeffreytang97@gmail.com>
2018-02-16 02:03:29 -0500

    All works fine now! Changed the index page to align it at center and made some small design change.

--- QA-webapp/app/models/question.rb
+++ QA-webapp/app/models/question.rb
@@ -1,5 +1,5 @@
 class Question < ApplicationRecord
-  has_many :answers, dependent: :destroy
-  validates :title, presence: true, length: { minimum: 10, maximum: 50 }
-  validates :text, presence: true, length: { minimum: 10, maximum: 300 }
+  	has_many :answers, dependent: :destroy
+  	validates :title, presence: true, length: { minimum: 10, maximum: 50 }
+  	validates :text, presence: true, length: { minimum: 10, maximum: 300 }
 end
