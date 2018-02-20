794229c60ebf5199ba7ef50ffc9c88683ababf78
Samuel Gosselin <samuelgosselin1@hotmail.com>
2018-02-20 17:53:11 -0500

    Added user association to question & asked by on listing

--- QA-webapp/app/models/question.rb
+++ QA-webapp/app/models/question.rb
@@ -2,4 +2,12 @@ class Question < ApplicationRecord
   	has_many :answers, dependent: :destroy
   	validates :title, presence: true, length: { minimum: 10, maximum: 50 }
   	validates :text, presence: true, length: { minimum: 10, maximum: 300 }
+    
+    def user
+      if self.user_id.blank?
+        return nil
+      else
+        User.find(self.user_id)
+      end
+    end
 end
--- QA-webapp/db/schema.rb
+++ QA-webapp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180214181135) do
+ActiveRecord::Schema.define(version: 20180220220055) do
 
   create_table "answers", force: :cascade do |t|
     t.string "user"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 20180214181135) do
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_questions_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
