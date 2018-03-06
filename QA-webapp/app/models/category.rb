d09748086d96a7d82cb0cf676f69902b492d495a
Avkash Sharma <avkaashsharma89@gmail.com>
2018-03-06 14:52:50 -0500

    pagination

--- QA-webapp/app/models/category.rb
+++ QA-webapp/app/models/category.rb
@@ -1,4 +1,4 @@
 class Category < ApplicationRecord
-  validates :name, presence: true, length: {minimum: 5, maximum: 25 }
+  validates :name, presence: true, length: {minimum: 3, maximum: 25 }
   validates_uniqueness_of :name
 end
