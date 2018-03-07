2812fd27bbef4fb873fbff382cae2d424d2e66a6
Avkash Sharma <avkaashsharma89@gmail.com>
2018-03-07 14:12:35 -0500

    email validation

--- QA-webapp/config/routes.rb
+++ QA-webapp/config/routes.rb
@@ -4,9 +4,6 @@ Rails.application.routes.draw do
   get 'sessions/success'
   post 'sessions' => 'sessions#create'
   delete 'sessions' => 'sessions#destroy'
-  
-  get 'signup', to: 'users#new'
-  resources :users, except: [:new]
 
   resources :users
   root 'welcome#index', as: 'home'
