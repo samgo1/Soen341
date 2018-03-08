f67569a731e0b7641a4b99878a38fed970ff3e60
Seb_desla <seb.lacrosse@gmail.com>
2018-03-07 19:50:58 -0500

    We can now reject naswers, if the downvotes are equal to 60% of total votes

--- QA-webapp/app/controllers/answers_controller.rb
+++ QA-webapp/app/controllers/answers_controller.rb
@@ -28,12 +28,27 @@ class AnswersController < ApplicationController
         redirect_to request.referrer 
     end
 
+
     def downvote 
         @answer.downvote_from current_user
-        redirect_to request.referrer
+        if(answer_rejected?)
+            @answer.destroy
+            redirect_to request.referrer
+        else
+            redirect_to request.referrer
+        end
     end
 
     private
+        def answer_rejected?
+            @total = @answer.cached_votes_total
+            if(@total > 3 && @answer.cached_votes_down > 0.6 * @total)
+                return true
+            else
+                return false
+            end
+                
+        end
 
         def set_answer
             @answer = Answer.find(params[:id])
